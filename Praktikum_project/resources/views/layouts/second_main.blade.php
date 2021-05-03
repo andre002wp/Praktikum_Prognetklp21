7358c9627ea0437deee30c838a0a7c5a581767c5
andre002wp <andre002wp@gmail.com>
2021-05-03 17:36:47 +0800

    Update second_main.blade.php

--- Praktikum_project/resources/views/layouts/second_main.blade.php
+++ Praktikum_project/resources/views/layouts/second_main.blade.php
@@ -42,8 +42,9 @@
                         </li>
                     @endif
                 @else
+                    
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown-menu" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
 
