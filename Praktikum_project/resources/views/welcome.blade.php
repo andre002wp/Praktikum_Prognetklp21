40196dce305a2b8838fd051c443efcb5d5abffda
andre002wp <andre002wp@gmail.com>
2021-05-11 02:30:39 +0800

    update

--- Praktikum_project/resources/views/welcome.blade.php
+++ Praktikum_project/resources/views/welcome.blade.php
@@ -1,1 +1,9 @@
 @extends('admin.layouts.master')
+
+@section('content')
+    <div class="content-wrapper">
+        <div class="container">
+        ini welcome.blade.php
+        </div>
+    </div>
+@endsection
