0b7adfaa71e3bef09b0cb289d1f5a88e124ac2c9
andre002wp <andre002wp@gmail.com>
2021-06-01 00:07:19 +0800

    ap

--- Praktikum_project/resources/views/notif.blade.php
+++ Praktikum_project/resources/views/notif.blade.php
@@ -9,9 +9,9 @@
             <li class="hassubs active">
                 <?php 
                     $id = Auth::user()->id;
-                    $notif_count = Auth()->user()->unreadNotifications->count();
-                    $notifications = Auth::user()->notifications;
-                    // $notifications = DB::table('user_notifications')->where('notifiable_id',$id)->where('read_at',NULL)->orderBy('created_at','desc')->get();
+                    // $notif_count = Auth()->user()->unreadNotifications->count();
+                    // $notifications = Auth::user()->notifications;
+                    $notifications = DB::table('user_notifications')->where('notifiable_id',$id)->where('read_at',NULL)->orderBy('created_at','desc')->get();
                 ?>
                 {{-- <a href="/home"><span class="badge badge-pill badge-danger">{{$notif_count}}</span></a> --}}
                 <ul >
