e3d5f7fcf6eb862c1f4f2db353ffd4c588b5f144
andre002wp <andre002wp@gmail.com>
2021-05-31 19:05:01 +0800

    Delete Praktikum_project directory

--- Praktikum_project/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admin.layouts.master')
-
-@section('content')
-    <div class="content-wrapper">
-        <div class="container">
-        ini welcome.blade.php
-        </div>
-    </div>
-@endsection
