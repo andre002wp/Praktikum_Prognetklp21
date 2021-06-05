2ee5175d206125adbc0dc0456731a24a015a3947
taraadriana <80681345+taraadriana@users.noreply.github.com>
2021-06-05 17:52:31 +0700

    ghggh

--- Praktikum_project/app/Http/Controllers/Admin/AdminDetailTransaksiController.php
+++ Praktikum_project/app/Http/Controllers/Admin/AdminDetailTransaksiController.php
@@ -37,36 +37,36 @@ class AdminDetailTransaksiController extends Controller
         if($request->status == 1){
             $transaksi->status = 'canceled';
             $transaksi->save();
-            $user->notify(new UserStatusTransactionChanged($transaksi->id, $transaksi->status, 'canceled'));
-            return redirect('transaksi/detail/'.$request->id);
+            //$user->notify(new UserStatusTransactionChanged($transaksi->id, $transaksi->status, 'canceled'));
+            return redirect('/admin/transaksi');
         
         }elseif($request->status == 2){
             $transaksi->status = 'success';
             $transaksi->save();
-            $user->notify(new UserStatusTransactionChanged($transaksi->id, $transaksi->status, 'success'));
-            $allAdmins = Admin::all();
-            foreach ($allAdmins as $it) {
-                $it->notify(new PaymentUploaded($transaksi->id));
-            }
-            return redirect('transaksi/detail/'.$request->id);
+            //$user->notify(new UserStatusTransactionChanged($transaksi->id, $transaksi->status, 'success'));
+            // $allAdmins = Admin::all();
+            // foreach ($allAdmins as $it) {
+            //     $it->notify(new PaymentUploaded($transaksi->id));
+            // }
+            return redirect('/admin/transaksi');
 
         }elseif($request->status == 3){
             $transaksi->status = 'verified';
-            $user->notify(new UserStatusTransactionChanged($transaksi->id, $transaksi->status, 'verified'));
+$user->notify(new UserStatusTransactionChanged($transaksi->id, $transaksi->status, 'verified'));
             $transaksi->save();
 
-            foreach($transaksi->transaction_details as $data){
-                $produk = Product::find($data->product_id);
-                $produk->stock = $produk->stock - $data->qty;
-                $produk->save();
-            }
-            return redirect('transaksi/detail/'.$request->id);
+            // foreach($transaksi->transaction_details as $data){
+            //     $produk = Product::find($data->product_id);
+            //     $produk->stock = $produk->stock - $data->qty;
+            //     $produk->save();
+            // }
+            return redirect('/admin/transaksi');
             
         }else{
             $transaksi->status = 'delivered';
             $transaksi->save();
-            $user->notify(new UserStatusTransactionChanged($transaksi->id, $transaksi->status, 'delivered'));
-            return redirect('transaksi/detail/'.$request->id);
+            //$user->notify(new UserStatusTransactionChanged($transaksi->id, $transaksi->status, 'delivered'));
+           return redirect('/admin/transaksi');
         }
     }
 }
